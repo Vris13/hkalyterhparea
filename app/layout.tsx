e38a1f594d2c161f0bb9c442f093a8144af3cf07
Georgios Vris <grgvris@gmail.com>
2026-01-06 17:07:28 +0200

    New Favicon, correctly added

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,13 @@ export const metadata: Metadata = {
         url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍷</text></svg>',
       },
     ],
+    apple: [
+      {
+        url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍷</text></svg>',
+      },
+    ],
   },
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
